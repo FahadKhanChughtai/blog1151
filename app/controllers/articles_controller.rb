7d1d952585094189a6fcec893a08d56ab2f3c4d8
YOUR NAME <fahadkhanchughtai@gmail.com>
2020-08-09 16:22:02 +0500

    form edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,36 @@
 class ArticlesController < ApplicationController
 	def index
-	@articles = Article.all
+		@articles = Article.all
 	end
 	def show
-    @article = Article.find(params[:id])
+    	@article = Article.find(params[:id])
   	end
 	def new
 		@article = Article.new
 	end
+	def edit
+  		@article = Article.find(params[:id])
+	end
 	def create
-	@article = Article.new(params.require(:article).permit(:title, :text)) 
- 	if @article.save
-    redirect_to @article
- 	else
-    render 'new'
- 	end
-end
+		@article = Article.new(params.require(:article).permit(:title, :text)) 
+	 	if @article.save
+	    redirect_to @article
+	 	else
+	    render 'new'
+	 	end
+	end
+	def update
+	  @article = Article.find(params[:id])
+	 
+	  if @article.update(article_params)
+	    redirect_to @article
+	  else
+	    render 'edit'
+	  end
+	end
+	
   	private
   	def article_params
-    params.require(:article).permit(:title, :text)
+   	 params.require(:article).permit(:title, :text)
   	end
 end
