b644c8d00f7cba1539600c63a98a5a971f11153a
YOUR NAME <fahadkhanchughtai@gmail.com>
2020-08-09 14:18:13 +0500

    form validation applied

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,13 +6,16 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   	end
 	def new
+		@article = Article.new
 	end
 	def create
 	@article = Article.new(params.require(:article).permit(:title, :text)) 
- 	@article.save
-  	redirect_to @article
-  	end
-
+ 	if @article.save
+    redirect_to @article
+ 	else
+    render 'new'
+ 	end
+end
   	private
   	def article_params
     params.require(:article).permit(:title, :text)
